4247cadc8d47f139709b8e7ff64dd96026b54cb0
Laszlo Bombolya <laszlo.bombolya@codecool.com>
2023-12-12 09:42:21 +0100

    Add starter

---
+++ src/decipher.cpp
@@ -0,0 +1,5 @@
+#include "decipher.h"
+
+std::string decipher(std::string_view key, std::string_view codedMessage) {
+    return {};
+}
---
+++ src/decipher.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+std::string decipher(std::string_view key, std::string_view codedMessage);
+
